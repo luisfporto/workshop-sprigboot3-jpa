89ed7e66ca17b7d8d49980a519b99ad197634acf
Luis Porto <luisporto1717@gmail.com>
2023-12-27 21:20:42 +0000

    User and resource

---
+++ src/main/java/com/lp/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.lp.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lp.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "123452");
+		
+		return ResponseEntity.ok().body(u);
+	}
+}
