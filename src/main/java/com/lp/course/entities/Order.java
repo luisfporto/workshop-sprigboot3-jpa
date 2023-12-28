45d578b4682fdb9dd6d45590d35f03bd2b1d28a4
Luis Porto <luisporto1717@gmail.com>
2023-12-28 22:44:56 +0000

    Categories

--- src/main/java/com/lp/course/entities/Order.java
+++ src/main/java/com/lp/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.lp.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -24,6 +25,7 @@ public class Order implements Serializable {
 	private Long id;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd 'T' HH:mm:ss 'Z'", timezone = "GMT")
 	private Instant moment;
+	private Integer orderStatus;
 	@ManyToOne /* ASSOCIAÇÃO MUITOS(PEDIDOS) PARA UM */
 	@JoinColumn(name = "client_id") /* CLIENT_ID É A CHAVE ESTRANGEIRA, QUE TERÁ NO BANCO DE DADOS */
 	private User client;
@@ -65,6 +67,16 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+
 	public User getClient() {
 		return client;
 	}
@@ -73,10 +85,11 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
