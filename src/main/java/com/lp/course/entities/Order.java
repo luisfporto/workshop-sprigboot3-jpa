f46aaa84bc2a15ce48ad041983f20e5ef840eb75
Luis Porto <luisporto1717@gmail.com>
2023-12-28 16:05:11 +0000

    Order, instant and lessons

--- src/main/java/com/lp/course/entities/Order.java
+++ src/main/java/com/lp/course/entities/Order.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,16 +15,17 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name ="tb_order")
+@Table(name = "tb_order")
 public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	@Id /* ID INDICA QUAL A CHAVE PRIMÁRIA DA TABELA, NO BANCO DE DADOS*/
+	@Id /* ID INDICA QUAL A CHAVE PRIMÁRIA DA TABELA, NO BANCO DE DADOS */
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd 'T' HH:mm:ss 'Z'", timezone = "GMT")
 	private Instant moment;
-	@ManyToOne /*ASSOCIAÇÃO MUITOS(PEDIDOS) PARA UM */
-	@JoinColumn(name = "client_id") /*CLIENT_ID É A CHAVE ESTRANGEIRA, QUE TERÁ NO BANCO DE DADOS*/
+	@ManyToOne /* ASSOCIAÇÃO MUITOS(PEDIDOS) PARA UM */
+	@JoinColumn(name = "client_id") /* CLIENT_ID É A CHAVE ESTRANGEIRA, QUE TERÁ NO BANCO DE DADOS */
 	private User client;
 
 	public Order() {
