693fa9d451f311102714c0d56bb1351eba6e54af
Luis Porto <luisporto1717@gmail.com>
2024-01-01 12:06:24 +0000

    Exceptions treatment findById

---
+++ src/main/java/com/lp/course/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.lp.course.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.lp.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	/*
+	 * ResponseEntity retrieves the complete Http response, as body status code and
+	 * headers According to most part of the videos, this method will be used to get
+	 * the response
+	 */
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandartError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		/*
+		 * O status.value() se faz necessário pois a variável "status" não tem um
+		 * construtor que remeta diretamente a ela, sendo assim, é necessário buscar
+		 * dentro do método
+		 */
+		return ResponseEntity.status(status).body(err);
+	}
+}
