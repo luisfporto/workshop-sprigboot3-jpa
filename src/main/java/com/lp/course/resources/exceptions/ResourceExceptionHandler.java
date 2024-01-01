2d034b69a10bd14f6ff1d6f964a5b47d2ff83024
Luis Porto <luisporto1717@gmail.com>
2024-01-01 12:25:44 +0000

    Delete exception and Db Integrity

--- src/main/java/com/lp/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/lp/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.lp.course.services.exceptions.DatabaseException;
 import com.lp.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,6 +23,20 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandartError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		/*
+		 * O status.value() se faz necessário pois a variável "status" não tem um
+		 * construtor que remeta diretamente a ela, sendo assim, é necessário buscar
+		 * dentro do método
+		 */
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandartError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getMessage(),
 				request.getRequestURI());
 		/*
--- src/main/java/com/lp/course/resources/exceptions/StandartError.java
+++ src/main/java/com/lp/course/resources/exceptions/StandartError.java
@@ -3,8 +3,11 @@ package com.lp.course.resources.exceptions;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class StandartError implements Serializable {
 	private static final long serialVersionUID = 1L;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd 'T' HH:mm:ss 'Z'", timezone = "GMT")
 	private Instant timestamp;
 	private Integer status;
 	private String error;
